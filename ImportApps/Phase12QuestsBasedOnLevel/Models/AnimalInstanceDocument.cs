af75e31eafe598f7848006128aeeef0f7c945a90
Andy Walter <musictopia2@hotmail.com>
2026-01-17 11:44:48 -0700

    Removed the projects unrelated to the instant unlimited.

--- ImportApps/Phase12QuestsBasedOnLevel/Models/AnimalInstanceDocument.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Phase12QuestsBasedOnLevel.Models;
-public class AnimalInstanceDocument
-{
-    required public BasicList<AnimalAutoResumeModel> Animals { get; set; }
-    required public FarmKey Farm { get; set; }
-}
--- WebApps/Phase01SpeedSeeds/DataAccess/Worksites/WorksiteInstanceDocument.cs
+++
@@ -1,8 +0,0 @@
-﻿using Phase01SpeedSeeds.Services.Core;
-
-namespace Phase01SpeedSeeds.DataAccess.Worksites;
-public class WorksiteInstanceDocument : IFarmDocument
-{
-    required public FarmKey Farm { get; set; }
-    required public BasicList<WorksiteAutoResumeModel> Worksites { get; set; } = [];
-}
